555b65fecce6a0d80d02dde3d975bf446d8b0ece
Torsten Römer <torsten.roemer@luniks.net>
2025-04-13 16:15:29 +0200

    Incorporate changes from librfm95 (#3)
    * Incorporate changes from librfm95
    * Adjustments after rename to librfm69
    - change handling of timeout interrupt - to be tested
    * Rename constants, make bcast address configurable, F_DIV
    * Some cleanup
    * Test & adjust for RFM69
    * Always disable/reset timeout
    * Make sure radio is in standby mode

--- nbproject/Package-Custom.bash
+++ nbproject/Package-Custom.bash
@@ -13,9 +13,9 @@ CND_BUILDDIR=build
 CND_DLIB_EXT=so
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/librfm
-OUTPUT_BASENAME=librfm
-PACKAGE_TOP_DIR=librfm/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/librfm69
+OUTPUT_BASENAME=librfm69
+PACKAGE_TOP_DIR=librfm69/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/librfm/bin"
+makeDirectory "${NBTMPDIR}/librfm69/bin"
 copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/librfm.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/librfm69.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/librfm.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/librfm69.tar *
 checkReturnCode
 
 # Cleanup
